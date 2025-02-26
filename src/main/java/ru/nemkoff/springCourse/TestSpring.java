101c23d20b5b3ef3e3c68e660e44c1c6216e46c8
Nemkoff <nemkov_work@mail.ru>
2025-02-26 15:24:42 +0300

    6. Annotations. Part 1: Component, Autowired.

--- src/main/java/ru/nemkoff/springCourse/TestSpring.java
+++ src/main/java/ru/nemkoff/springCourse/TestSpring.java
@@ -2,20 +2,26 @@ package ru.nemkoff.springCourse;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * @author Neil Alishev
- */
 public class TestSpring {
   public static void main(String[] args) {
     ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
         "applicationContext.xml"
     );
 
-    Music music = context.getBean("musicBean", Music.class);
+//    Music music = context.getBean("rockMusic", Music.class);
+//
+//    MusicPlayer musicPlayer = new MusicPlayer(music);
+//    musicPlayer.playMusic();
+//
+//    Music music2 = context.getBean("classicalMusic", Music.class);
+//    MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+//    musicPlayer2.playMusic();
+//
+//    MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//    musicPlayer.playMusic();
 
-    MusicPlayer musicPlayer = new MusicPlayer(music);
-    musicPlayer.playMusic();
-
+    Computer computer = context.getBean("computer", Computer.class);
+    System.out.println(computer);
     context.close();
   }
 }
